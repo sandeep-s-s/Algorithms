b0c756293691ffc343943a509f465d820953f57f
sandeep s s <sandeepsreekumar75@gmail.com>
2018-07-28 22:32:47 +0530

    Travel of an array implemented

--- Traversal.c
+++ Traversal.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 int main()
 {
-	int i;
-	int n;
+	int i,n;
 	int array[10];
-	printf("Enter array limit : ");
-	scanf("%d",&n);
-	printf("Enter array elements : ");
+	printf("Enter array limit : "); 
+	scanf("%d",&n); /* Read array limit */
+	printf("Enter array elements : "); 
 	for (int i = 0; i < n; ++i){
-		scanf("%d",&array[i]);
+		scanf("%d",&array[i]); /* Read array elements */
 	}
 
-	for (int i = 0; i < n; ++i)
-	{
-		printf("%d Visited \n", array[i]);
+	for (int i = 0; i < n; ++i){
+		printf("%d Visited \n", array[i]); /* Display array elements */
 	}
 	return 0;
 }

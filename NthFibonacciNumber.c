67a12fdb66f96d0a3d02fc2e96212ec5dc34c2c2
Sandeep <sandeepsreekumar75@gmail.com>
2019-02-12 09:54:36 +0530

    Recursive algorithm for nth fibonacci number is implemented

---
+++ NthFibonacciNumber.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int fibonacci(int n)
+{
+	if(n<=1)
+		return(n);
+	return (fibonacci(n-1)+fibonacci(n-2));
+}
+int main()
+{
+	int n;
+	printf("Enter the limit : ");
+	scanf("%d",&n);
+	printf("Output : %d",fibonacci(n));
+}

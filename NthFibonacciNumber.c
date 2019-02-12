c5d024c33fd1f8efb0016b85817448c5cc794762
Sandeep <sandeepsreekumar75@gmail.com>
2019-02-12 10:40:19 +0530

    bug fix

--- NthFibonacciNumber.c
+++ NthFibonacciNumber.c
@@ -1,14 +1,24 @@
 #include<stdio.h>
-int fibonacci(int n)
+int fibonacci(int n) //recursive function for computing nth fibonacci number
 {
 	if(n<=1)
 		return(n);
 	return (fibonacci(n-1)+fibonacci(n-2));
 }
+
+int isPositiveNumber(int n)
+{
+	return n>0;
+}
+
 int main()
 {
 	int n;
-	printf("Enter the limit : ");
+	printf("Input number : ");
 	scanf("%d",&n);
-	printf("Output : %d",fibonacci(n));
+	if(isPositiveNumber(n))
+		printf("%d th fibonacci number : %d \n",n,fibonacci(n));
+	else
+		printf("Expected number is be Postive\n");
+	return 0;
 }

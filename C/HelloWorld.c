a80b90fbbe73cf085f6b4a7b9ba0fe3ff6e72e2c
Sandeep <sandeepsreekumar75@gmail.com>
2019-02-14 00:23:18 +0530

    bug fix

--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-int
-main (void)
+int main (void)
 {
   printf ("Welcome to the world of C\n");
   return 0;
--- HelloWorld.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int
-main (void)
-{
-  printf ("Welcome to the world of C\n");
-  return 0;
-}

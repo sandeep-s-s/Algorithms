9ea178038c084b344bd3a8d9c131cd120f74acc2
sandeep s s <sandeepsreekumar75@gmail.com>
2020-12-04 23:44:23 +0530

    [Feature] ✨ Array Delete operation

---
+++ C/array/Delete.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int i,n,ele,x;
+	int array[10]={6,7,8,9,10};
+	n=5;
+
+	printf("Initial array elements ");
+	
+	for (int i = 0; i < n; ++i){
+		printf("%d ", array[i]); /* Array elements are :*/
+	}
+
+	printf("\nEnter position of element to delete : ");
+	scanf("%d",&x);
+
+	if(x>n){
+		printf("Array index out of bound \n");
+		exit(0);
+	}
+
+	while(n-1 > x-1){               
+	    array[x-1]=array[x];
+	    x++;
+	}
+	--n;
+	
+	printf("Final array : ");
+	for(int i=0; i<n;i++){
+	    printf("%d ", array[i]); 
+	}
+	printf("\n");
+	return 0;
+}
